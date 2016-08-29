d1e52b9a7fb1cbdb96a67139a0fe08d2f82a14f7
wjtshitc123 <307439239@qq.com>
2016-08-29 15:20:11 +1000

    Add files via upload

---
+++ home.php
@@ -0,0 +1,108 @@
+<html>
+  
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css"
+    rel="stylesheet" type="text/css">
+    <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css"
+    rel="stylesheet" type="text/css">
+  </head>
+  
+  <body>
+    <div class="navbar navbar-default" style="background-color:white">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <img style="width:250px;height:50" src="Logo.png">
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-ex-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active">
+              <a href="http://localhost/iteration1/home.php">Home</a>
+            </li>
+            <li>
+              <a href="http://localhost/iteration1/statistic.php">Statistics</a>
+            </li>
+            <li>
+              <a href="http://localhost/iteration1/self-evaluation.php">Self-Evaluation</a>
+            </li>
+            <li>
+              <a href="http://localhost/iteration1/forum.php">User Discassion Forum</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="carousel slide" id="carousel-example" data-interval="false"
+    data-ride="carousel">
+      <div class="carousel-inner" >
+        
+          <div class="item active">
+          <a href="http://localhost/iteration1/home.php">
+            <img src="workeralone.jpg" style="height:650px">
+          </a>
+
+          <div class="carousel-caption">
+            <h2>SAFE@PTJ</h2>
+            <p>safeatpart-timejob.tk</p>
+          </div>
+          </div>
+          <div class="item">
+          <a href="http://localhost/iteration1/statistic.php">
+            <img src="5.jpg" style="height:650px">
+          </a>
+
+          <div class="carousel-caption">
+            <h2>Statistic</h2>
+            <p>The statistic section can provide you the useful information like the average salary by occupation. Thus you can avoid the potential loss at work.Besides,  the ststistic of injury by industry will also be presented. You can use this information as a reference to choose your future career.</p>
+          </div>
+          </div>
+          <div class="item">
+          <a href="http://localhost/iteration1/self-evaluation.php">
+            <img src="7.jpg" style="height:650px">
+          </a>
+            <div class="carousel-caption">
+            <h2>Self-Evaluation</h2>
+            <p>You can also take the mini-test to check whether you have enough knowlwdge to handle your job. Some tips according to your anster will be displayed after you submitting your answer.</p>
+          </div>
+          </div>
+          <div class="item">
+          <a href="http://localhost/iteration1/forum.php">
+            <img src="10.jpg" style="height:650px">
+          </a>
+                    <div class="carousel-caption">
+            <h2>User Communication Forum</h2>
+            <p>Another important function is the user discussion forum. This function can help you commucate with others after your registeration.</p>
+          </div>
+          </div>
+      </div>
+      <a class="left carousel-control" href="#carousel-example" data-slide="prev"><i class="icon-prev  fa fa-angle-left"></i></a>
+      <a class="right carousel-control" href="#carousel-example" data-slide="next"><i class="icon-next fa fa-angle-right"></i></a>
+    </div>
+    <footer style="background-color:white">
+      <ul class="nav nav-pills" style="font-size:12px;">
+        <li>
+          <a href="http://localhost/iteration"></a>
+        </li>
+        <li style="margin-right:30px;">
+          <a href="http://localhost/iteration1/home.php">HOME</a>
+        </li>
+        <li style="margin-right:30px;">
+          <a href="http://localhost/iteration1/faqs.php">FAQS</a>
+        </li>
+        <li style="margin-right:30px;">
+          <a href="http://localhost/iteration1/aboutus.php">ABOUT US</a>
+        </li>
+      </ul>
+    </footer>
+  </body>
+
+</html>
